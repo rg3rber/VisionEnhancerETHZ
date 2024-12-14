480d33c417e62b85e69f977ec08cae88de68855e
mickey1356 <jeremycwj@gmail.com>
2024-12-14 20:42:17 +0100

    ocr + ip settings

--- Assets/OCR/ImageSender.cs
+++ Assets/OCR/ImageSender.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using TMPro;
 
 [System.Serializable]
 public class ImageRequestData
@@ -30,10 +33,26 @@ public static class UnityWebRequestExtensions
 
 public class ImageSender : MonoBehaviour
 {
+    [SerializeField] private GameObject webcamObj;
     [SerializeField] private TextToSpeechManager ttsManager;
-    [SerializeField] private LoadingScreenManager loadingScreenManager;
-    [SerializeField] private string serverUrl = "http://localhost:5000/process_image";
+    [SerializeField] private TMP_InputField iptext;
+    [SerializeField] private GameObject ipObj;
+    [SerializeField] private GameObject hideBtn;
+    // [SerializeField] private string serverUrl = "http://localhost:5000/process_image";
     private float processingStartTime;
+
+    private string GOOGLE_VISION_API_KEY = "AIzaSyBvlLMD5Ys9MJ_3zkEr9CfNGuTYZ7NB2RQ";
+    private string GOOGLE_VISION_API_URL = "https://vision.googleapis.com/v1/images:annotate";
+
+
+    public void ToggleWebcam() {
+        webcamObj.SetActive(!webcamObj.activeSelf);
+    }
+
+    public void ToggleSettings() {
+        ipObj.SetActive(!ipObj.activeSelf);
+        hideBtn.SetActive(!hideBtn.activeSelf);
+    }
 
     private void Start()
     {
@@ -41,10 +60,64 @@ public class ImageSender : MonoBehaviour
         {
             Debug.LogError("TextToSpeechManager reference not set in ImageSender");
         }
-        if (loadingScreenManager == null)
+        // if (loadingScreenManager == null)
+        // {
+        //     Debug.LogError("LoadingScreenManager reference not set in ImageSender");
+        // }
+    }
+
+    public IEnumerator DirectSendImage(Texture2D texture) {
+        processingStartTime = Time.time;
+        if (texture == null)
         {
-            Debug.LogError("LoadingScreenManager reference not set in ImageSender");
+            Debug.LogError("Texture is null in SendImageToServer");
+            yield break;
         }
+
+        ttsManager?.SpeakText("Processing image");
+        byte[] imageBytes = texture.EncodeToPNG();
+        string base64Image = System.Convert.ToBase64String(imageBytes);
+        string jsonData = JsonUtility.ToJson(new ImageRequestData { image = base64Image });
+
+        string greq = "{\"requests\": [{\"features\": [{\"type\":\"TEXT_DETECTION\"}], \"image\":{\"content\": \"" + base64Image + "\"}}]}";
+
+        Debug.Log(greq);
+
+        // send to google vision (ignore chatgpt stuff for now)
+        using (UnityWebRequest request = new UnityWebRequest(GOOGLE_VISION_API_URL + "?key=" + GOOGLE_VISION_API_KEY, "POST")) {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(greq);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+
+            request.SetRequestHeader("Content-Type", "application/json");
+            Debug.Log("Sending image to server...");
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Image sent successfully!");
+                string response = request.downloadHandler.text;
+                Debug.Log(response);
+                Regex regex = new Regex("\"description\": \"(.*)\"");
+                Match m = regex.Match(response);
+                if (m.Success) {
+                    ttsManager?.SpeakText(m.Groups[1].ToString());
+                } else {
+                    ttsManager?.SpeakText("No text found");
+                }
+                // Debug.Log(regex.Match(response).Groups[1]);
+                // if (Regex.M)
+                // ProcessServerResponse(response);
+            }
+            else
+            {
+                string errorMessage = $"Error sending image: {request.error}";
+                Debug.LogError(errorMessage);
+                ttsManager?.SpeakText(errorMessage);
+            }
+
+        }
+
     }
 
     public IEnumerator SendImageToServer(Texture2D texture)
@@ -56,10 +129,10 @@ public class ImageSender : MonoBehaviour
             yield break;
         }
 
-        if (loadingScreenManager != null)
-        {
-            loadingScreenManager.ShowLoadingScreen();
-        }
+        // if (loadingScreenManager != null)
+        // {
+        //     loadingScreenManager.ShowLoadingScreen();
+        // }
 
         ttsManager?.SpeakText("Processing image");
 
@@ -67,7 +140,9 @@ public class ImageSender : MonoBehaviour
         string base64Image = System.Convert.ToBase64String(imageBytes);
         string jsonData = JsonUtility.ToJson(new ImageRequestData { image = base64Image });
 
-        using (UnityWebRequest request = new UnityWebRequest(serverUrl, "POST"))
+        Debug.Log("http://" + iptext.text + "/process_image");
+
+        using (UnityWebRequest request = new UnityWebRequest("http://" + iptext.text + "/process_image", "POST"))
         {
             byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
             request.uploadHandler = new UploadHandlerRaw(bodyRaw);
@@ -77,10 +152,10 @@ public class ImageSender : MonoBehaviour
             Debug.Log("Sending image to server...");
             yield return request.SendWebRequest();
 
-            if (loadingScreenManager != null)
-            {
-                loadingScreenManager.HideLoadingScreen();
-            }
+            // if (loadingScreenManager != null)
+            // {
+            //     loadingScreenManager.HideLoadingScreen();
+            // }
 
             if (request.result == UnityWebRequest.Result.Success)
             {
